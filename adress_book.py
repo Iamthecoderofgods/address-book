7e99aaf0f048d82c97ad25598d41a719e388eaeb
Iamthecoderofgods <22mjames@strichardgwyn.school>
2026-03-17 18:56:45 +0000

    Add files via upload

--- adress_book.py
+++ adress_book.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter.filedialog import *
+from tkinter import messagebox
 
 root = Tk()
-root.geometry("500x500")
+root.geometry("500x600")
 root.title("The book of addresses")
 
 contacts = {}
@@ -23,13 +24,84 @@ Mobile_text = Entry(root)
 Email_text = Entry(root)
 Birthday_text = Entry(root)
 
-def Save():
-    contacts[Name]
+def Update():
+    name = Name_text.get()
+    
+    if not name:
+        messagebox.showwarning("Error!!","you are a stupid idiot for putting no name in the box you stupid idoit")
+    
+        return
+
+    Name_text.delete(0,END)
+
+    Address_text.delete(0,END)
+
+    Mobile_text.delete(0,END)
+
+    Email_text.delete(0,END)
+
+    Birthday_text.delete(0,END)
+    
+
+
+    contacts[name] = {"Address":Address_text.get(),
+                      "Mobile":Mobile_text.get(),
+                      "Email":Email_text.get(),
+                      "Birthday":Birthday_text.get()}
+    
+    if name not in Output_listbox_thingy.get(0,END):
+       Output_listbox_thingy.insert(END,name) 
+
+def edit():
+    selection = Output_listbox_thingy.curselection()
+
+    if not selection:
+        messagebox.showwarning("Error!!","you are a stupid idiot for not slecting. you have to select the person you stupid idiot",)
+        
+        return
+    name = Output_listbox_thingy.get(selection[0])
+    deatils = contacts[name]
+    Name_text.delete(0,END)
+    Name_text.insert(0,name)
+
+    Address_text.delete(0,END)
+    Address_text.insert(0,deatils["Address"])
+
+    Mobile_text.delete(0,END)
+    Mobile_text.insert(0,deatils["Mobile"])
+
+    Email_text.delete(0,END)
+    Email_text.insert(0,deatils["Email"])
+
+    Birthday_text.delete(0,END)
+    Birthday_text.insert(0,deatils["details"])
+
+def deletes():
+    selection = Output_listbox_thingy.curselection()
+    
+    if not selection:
+        messagebox.showwarning("Error!!","you are a stupid idiot for not slecting. you have to select the person you stupid idiot",)
+        
+        return
+    name = Output_listbox_thingy.get(selection[0])
+    del contacts[name]
+    Output_listbox_thingy.delete(selection[0])
 
 
 
-Save_button.config(command=Save)
 
+
+
+
+
+
+    
+
+
+
+
+Edit_button.config(command=edit)
+Update_button.config(command=Update)
 Output_listbox_thingy.grid(row=0,column=0,rowspan=10)
 Name.grid(row=0,column=1)
 Name_text.grid(row=0,column=2)
